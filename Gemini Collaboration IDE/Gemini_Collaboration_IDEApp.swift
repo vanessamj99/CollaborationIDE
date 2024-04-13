4faea05169e81d8eee22f54886de79aa7dfe708c
Vanessa Johnson <vmjohnson999@gmail.com>
2024-04-13 15:52:14 -0400

    Initial Commit

---
+++ Gemini Collaboration IDE/Gemini_Collaboration_IDEApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gemini_Collaboration_IDEApp.swift
+//  Gemini Collaboration IDE
+//
+//  Created by Vanessa Johnson on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Gemini_Collaboration_IDEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
